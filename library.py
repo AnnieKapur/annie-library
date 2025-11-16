1cae1dc4b61c7aae3a442ab03526e88fea720d42
AnnieKapur <annikakapur1996@gmail.com>
2025-11-16 09:59:04 +0000

    Added books with ISBN numbers

--- library.py
+++ library.py
@@ -1,2 +1,7 @@
-# WRITE CODE HERE
-
+library = {
+    "9780192833655": {"name": "The Picture of Dorian Gray", "quantity": 50},
+    "9780060173227": {"name": "To Kill a Mockingbird", "quantity": 3},
+    "9780140817744": {"name": "1984", "quantity": 7},
+    "9780192833983": {"name": "War and Peace", "quantity" : 10}
+    "9780805210408": {"name": "The Trial", "quantity" : 15} 
+}
